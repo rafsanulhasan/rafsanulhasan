15c1770a4d1f6dd680bb9939d39431887ef5dc51
rafsanulhasan <rafsanulhasan@outlook.com>
2020-11-17 02:25:55 +0600

    fixed lite project

--- sources/Applications/Web/SPA/Lite/Program.cs
+++ sources/Applications/Web/SPA/Lite/Program.cs
@@ -11,25 +11,23 @@ using Azure.Identity;
 
 namespace RH.Apps.Web.SPA.Lite
 {
-	public class Program
+	public static class Program
 	{
-		public static void Main(string[] args)
-		{
-			CreateHostBuilder(args).Build().Run();
-		}
+		public static void Main(string[] args) 
+			=> CreateHostBuilder(args).Build().Run();
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
-		    Host.CreateDefaultBuilder(args)
-.ConfigureAppConfiguration((context, config) =>
-{
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-config.AddAzureKeyVault(
-keyVaultEndpoint,
-new DefaultAzureCredential());
-})
-			   .ConfigureWebHostDefaults(webBuilder =>
-			   {
-				   webBuilder.UseStartup<Startup>();
-			   });
+			Host
+				.CreateDefaultBuilder(args)
+				.ConfigureAppConfiguration((_, config) =>
+				{
+					var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+					config.AddAzureKeyVault(
+					keyVaultEndpoint,
+					new DefaultAzureCredential());
+				})
+				.ConfigureWebHostDefaults(webBuilder
+					=> webBuilder.UseStartup<Startup>()
+				);
 	}
 }
