5102a4ddb1c559c57e5a9252576208f2fb0a85d4
rafsanulhasan <rafsanulhasan@outlook.com>
2020-11-17 02:25:43 +0600

    Fixed Server project

--- sources/Applications/Web/SPA/Server/Program.cs
+++ sources/Applications/Web/SPA/Server/Program.cs
@@ -1,27 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
+using Azure.Identity;
 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace RH.Apps.Web.SPA.Server
 {
-	public class Program
+	public static class Program
 	{
-		public static void Main(string[] args)
-		{
-			CreateHostBuilder(args).Build().Run();
-		}
+		public static void Main(string[] args) 
+			=> CreateHostBuilder(args).Build().Run();
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
-		    Host.CreateDefaultBuilder(args)
-			   .ConfigureWebHostDefaults(webBuilder =>
-			   {
-				   webBuilder.UseStartup<Startup>();
-			   });
+		     Host
+				.CreateDefaultBuilder(args)
+				.ConfigureAppConfiguration((_, config) =>
+				{
+					var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+						config.AddAzureKeyVault(
+						keyVaultEndpoint,
+					new DefaultAzureCredential());
+				})
+				.ConfigureWebHostDefaults(webBuilder => _ = webBuilder.UseStartup<Startup>());
 	}
 }
