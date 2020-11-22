416f6b6f8d1b6ced9a0e306a4022c689db322243
rafsanulhasan <rafsanulhasan@outlook.com>
2020-11-23 02:31:54 +0600

    Refactored the code

--- sources/Applications/Web/SPA/Client/Program.cs
+++ sources/Applications/Web/SPA/Client/Program.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -10,22 +9,23 @@ namespace RH.Apps.Web.SPA
 {
 	public static class Program
 	{
-		public static async Task Main(string[] args)
+		public static void Main(string[] args)
 		{
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("#app");
 
-			builder.Services.AddScoped(_
+			var services = builder.Services;
+
+			services.AddScoped(_
 				=> new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) }
 			);
 
 			// Configure your authentication provider options here.
 			// For more information, see https://aka.ms/blazor-standalone-auth
-			builder.Services.AddOidcAuthentication(options =>
+			services.AddOidcAuthentication(options =>
 				builder.Configuration.Bind("Local", options.ProviderOptions)
 			);
-
-			await builder.Build().RunAsync().ConfigureAwait(true);
+			builder.Build().RunAsync();
 		}
 	}
 }
