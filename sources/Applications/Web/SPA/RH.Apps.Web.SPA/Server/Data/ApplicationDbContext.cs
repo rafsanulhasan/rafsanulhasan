b35aff10687610c3d7013b0060284cb5b6165247
rafsanul Hasan <rafsanulhasan@outlook.com>
2021-02-02 03:43:23 +0600

    Created new asp.net core hosted blazor wasm project configured to run both as wasm PWA in client browser and server as well

---
+++ sources/Applications/Web/SPA/RH.Apps.Web.SPA/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using IdentityServer4.EntityFramework.Options;
+
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+using RH.Apps.Web.SPA.Server.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RH.Apps.Web.SPA.Server.Data
+{
+	public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+	{
+		public ApplicationDbContext(
+		    DbContextOptions options,
+		    IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+		{
+		}
+	}
+}
